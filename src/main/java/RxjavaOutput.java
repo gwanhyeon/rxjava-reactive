b6aabd0c7eccfa6fa44b98b77400cf6615f17198
kgh940525 <kgh940525@gmail.com>
2020-09-01 20:37:49 +0900

    Update getting started rxjava

--- src/main/java/RxjavaOutput.java
+++ src/main/java/RxjavaOutput.java
@@ -1,7 +1,8 @@
 import io.reactivex.Flowable;
-public class printTest {
+
+public class RxjavaOutput {
     public static void main(String[] args) {
-        Flowable<String> flowable = Flowable.just("version Info=>", "rxjava2.2.6, reactiveStreams1.0.2");
+        Flowable<String> flowable = Flowable.just("Hello", "World");
         flowable.subscribe(data-> System.out.println(data));
     }
 }

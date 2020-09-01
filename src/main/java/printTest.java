e405143aafb8e791c3d9f9e92b72f64e2cad7fca
kgh940525 <kgh940525@gmail.com>
2020-09-01 20:44:13 +0900

    Delete output testclass

--- src/main/java/printTest.java
+++
@@ -1,7 +0,0 @@
-import io.reactivex.Flowable;
-public class printTest {
-    public static void main(String[] args) {
-        Flowable<String> flowable = Flowable.just("version Info=>", "rxjava2.2.6, reactiveStreams1.0.2");
-        flowable.subscribe(data-> System.out.println(data));
-    }
-}
